c70442b97c1652f25d7a91699cf1852537508fde
Charlotte <charlotteleyser@icloud.com>
2024-03-25 18:06:44 +0100

    Updated source to image

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
                 <div id="main-flex">
                     <img
                     id="main-flex-img"
-                    src="/images/Headshot_CSL.png"
+                    src="../../public/images/Headshot_CSL.png"
                     alt="Headshot of Charlotte Swenning Leyser"
                     />
                     <h1 className="main-heading">Charlotte Swenning Leyser</h1>
